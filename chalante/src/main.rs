3f6b14d8732aba12b2bad48ecb83af1cf1d0aa5f
Carlos Salas Alarcón <110690378+csalas-alarcon@users.noreply.github.com>
2026-01-19 14:06:19 +0100

    Refactoring + Actual Learning

--- chalante/src/main.rs
+++ chalante/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event;
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(|mut terminal| {
+        loop {
+            terminal.draw(|frame| frame.render_widget("A MI DINA LA AMO INCLUSO MAS", frame.area()))?;
+            if event::read()?.is_key_press() {
+                break Ok(());
+            }
+        }
+    })
 }
